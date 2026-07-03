6b409f2d7e099eacf64323567001c3d7c7b08314
ivanjovicic <78588697+ivanjovicic@users.noreply.github.com>
2026-07-03 08:50:43 +0200

    Add SQLite-compatible API model options for relational tests

---
+++ tests/MathLearning.Tests/Helpers/SqliteApiDbContextOptions.cs
@@ -0,0 +1,30 @@
+using MathLearning.Domain.Entities;
+using MathLearning.Infrastructure.Persistance;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace MathLearning.Tests.Helpers;
+
+internal static class SqliteApiDbContextOptions
+{
+    public static DbContextOptions<ApiDbContext> Create(SqliteConnection connection)
+        => new DbContextOptionsBuilder<ApiDbContext>()
+            .UseSqlite(connection)
+            .ReplaceService<IModelCustomizer, SqliteCompatibleModelCustomizer>()
+            .Options;
+
+    private sealed class SqliteCompatibleModelCustomizer(
+        ModelCustomizerDependencies dependencies)
+        : ModelCustomizer(dependencies)
+    {
+        public override void Customize(ModelBuilder modelBuilder, DbContext context)
+        {
+            base.Customize(modelBuilder, context);
+
+            modelBuilder.Entity<QuestionStat>()
+                .Property(x => x.NextReview)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        }
+    }
+}
